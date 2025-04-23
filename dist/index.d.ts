51dec6e01cd1cd2eb602fa94043540e5e4a1d02f
Johan Tor <johan.tor@gmail.com>
2025-04-23 13:48:47 +0200

    feat: bump packages, add linting and build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,17 @@
-import MatchMediaAttributeManager from "./MatchMediaAttributeManager";
+declare class MatchMediaAttributeManager {
+    private controlelements;
+    private controlselector;
+    constructor(options?: MatchMediaAttributeManagerInitiationOptions);
+    private parseOptions;
+    InitiateElements(parent?: HTMLElement): void;
+    private bindEvents;
+    private onMatchChange;
+}
 export default MatchMediaAttributeManager;
+
+declare interface MatchMediaAttributeManagerInitiationOptions {
+    parent?: HTMLElement;
+    initiateElements?: boolean;
+}
+
+export { }
